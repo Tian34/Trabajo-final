25bfb1a079387ac7ddac46e328ad3e50f1b93694
Tian34 <70531781+Tian34@users.noreply.github.com>
2020-09-06 21:47:48 -0500

    Add files via upload

---
+++ bien.js
@@ -0,0 +1,31 @@
+
+var a = 0;
+var b = 1;
+var c = 2;
+var d = 3;
+
+var puntos = 0;
+puntos += 100;
+
+function siguiente() {
+	
+
+function sp() {
+         var aleatorio = Math.round(Math.random()*3);
+         console.log(aleatorio);
+         
+
+if (aleatorio==a) {window.location="p2.html"; a+=10;}
+else{}
+if (aleatorio==b) {window.location="p3.html"; b+=10;}
+else{}
+if (aleatorio==c) {window.location="p4.html"; c+=10;}
+else{}
+if (aleatorio==d) {window.location="p5.html"; d+=10;}
+else{}
+
+}
+
+sp();
+console.log(puntos)
+}
